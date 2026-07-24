590aeda3010a41a210bc49fa49aef0852f6112e4
Shahriar Hasan <shahriar@gmail.com>
2026-07-24 14:53:45 +0600

    Add HTMLMinifier service for publish pipeline

---
+++ apps/sites/services/html_minifier.py
@@ -0,0 +1,11 @@
+import re
+from bs4 import BeautifulSoup
+
+class HTMLMinifier:
+    def minify(self, html):
+        soup = BeautifulSoup(html, "html.parser")
+        repaired = str(soup.body or soup)
+
+        collapsed = re.sub(r">\s+<", "><", repaired)
+        collapsed = re.sub(r"\s{2,}", " ", repaired)
+        return collapsed.strip()
